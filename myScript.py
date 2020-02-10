350566e29daa6831e1cdccbb6b40bea8a1698c4d
Jostenn Herritt <jostenn_herritt@hotmail.com>
2020-02-10 11:32:14 -0800

    changing second_matrix

--- myScript.py
+++ myScript.py
@@ -1,6 +1,8 @@
 import numpy as np
+import pandas as pd
 
-my_matrix = np.identity(4)
-print(my_matrix)
+my_matrix = np.identity(5)
+my_second_matrix = np.random.random(size =(5,5))
+print(my_matrix, my_second_matrix)
 
 #this code is practice on uploading changes to github
