490e20f5422c082d7ba0ae985d1c89cee9740f84
Jostenn Herritt <jostenn_herritt@hotmail.com>
2020-02-10 11:21:49 -0800

    add my_func

--- myScript.py
+++ myScript.py
@@ -1,6 +1,11 @@
 import numpy as np
 
+def my_func(matrix_1, matrix_2)
+    return matrix_1 * matrix_2
+
 my_matrix = np.identity(4)
-print(my_matrix)
+my_second_matrix = np.identity(4)
+
+print(my_func(my_matrix, my_second_matrix))
 
 #this code is practice on uploading changes to github
